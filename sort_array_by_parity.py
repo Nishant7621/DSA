5188119f5da9e1e23170ae6014edabf5b8e24031
Nishant Jha <168707776+Nishant7621@users.noreply.github.com>
2025-10-02 22:01:54 +0530

    Add files via upload

---
+++ sort_array_by_parity.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def sortArrayByParity(self,nums: List[int]) -> List[int]:
+        n= len(nums)
+        start=0
+        for i in range(n):
+            if nums[i]%2==0:
+                temp=nums[i]
+                nums[i]=nums[start]
+                nums[start]=temp
+                start+=1
+        return nums
+
+obj=Solution()
+
+print(obj.sortArrayByParity([3,4,5,6,7,8,9,11,12]))
